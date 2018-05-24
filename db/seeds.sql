773d106c3ef2d7dd7871a6371ad3041cd53b0cef
Tom Knight <tomnite@gmail.com>
2018-05-23 21:45:56 -0400

    Trying to get Heroku to work

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (burger_name) VALUES ('Royale with cheese');
 INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Brett's Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Le Big Mac', true);
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
