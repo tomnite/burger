fad9fa3b63f9f0739c3f88773c26e02b3e2fb04a
Tom Knight <tomnite@gmail.com>
2018-05-23 21:08:15 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Royale with cheese');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
